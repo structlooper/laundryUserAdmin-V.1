38702800bf646c6759111b4d5ffd5ff24b531af3
Deepak kumar <structlooper@gmail.com>
2021-10-20 12:33:00 +0530

    final except otp

--- public/js/notification.js
+++ public/js/notification.js
@@ -1,15 +1,13 @@
 
 window.OneSignal = window.OneSignal || [];
 OneSignal.push(function() {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function() {
-        OneSignal.init({
-            appId: "76df60cb-3683-42c5-a78e-a0aae85206e3",
-            notifyButton: {
-                enable: true,
-            },
-            subdomainName: "kryche",
-        });
+    OneSignal.init({
+        appId: "76df60cb-3683-42c5-a78e-a0aae85206e3",
+        safari_web_id: "web.onesignal.auto.1b4e40e7-0d3a-47c7-b9a8-378d25a10892",
+        notifyButton: {
+            enable: true,
+        },
+        subdomainName: "kryche",
     });
 });
 
